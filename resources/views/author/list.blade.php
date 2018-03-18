9098adc88a2ead11b0ccfdf42029877bc5defae2
martian77 <eam31@yahoo.com>
2018-03-18 18:47:31 +0000

    Fixing empty user data.
    - Adding an add button to author and poem lists.
    - Adding some text if no poems or authors added.

--- resources/views/author/list.blade.php
+++ resources/views/author/list.blade.php
@@ -1,16 +1,33 @@
 @extends('layouts.content')
 
+@section('pagetitle')
+    <h1>{{ $pagetitle }}</h1>
+    @if (Auth::check())
+        <div class="actions">
+            <a class="btn btn-primary btn-sm" href="{{ route('author.add') }}">Add</a>
+        </div>
+    @endif
+@endsection
+
 @section('pagecontent')
     @unless(Auth::check())
         Please login to see a list of your authors.
     @else
-        <ul class="authors">
-        @foreach ($authors as $author)
-            <a href="{{ route('author', ['id' => $author->id]) }}">
-                <li class="author">
-                    <span class="author__firstname">{{ $author->firstname }}</span> <span class="author__familyname">{{ $author->familyname }}</span> <span class="author__preferredname">({{ $author->getPreferredName() }})</span>
-                </li>
-            </a>
-        @endforeach
+        @empty( $authors->count() )
+          <div class="no-authors information">
+            <p>You do not currently have any authors listed. Please add some!</p>
+          </div>
+        @else
+          <div>
+            <ul class="authors">
+            @foreach ($authors as $author)
+                <a href="{{ route('author', ['id' => $author->id]) }}">
+                    <li class="author">
+                        <span class="author__firstname">{{ $author->firstname }}</span> <span class="author__familyname">{{ $author->familyname }}</span> <span class="author__preferredname">({{ $author->getPreferredName() }})</span>
+                    </li>
+                </a>
+            @endforeach
+          </div>
+        @endempty
     @endunless
 @endsection
--- resources/views/poem/list.blade.php
+++ resources/views/poem/list.blade.php
@@ -1,24 +1,39 @@
 @extends('layouts.content')
 
+@section('pagetitle')
+    <h1>{{ $pagetitle }}</h1>
+    @if (Auth::check())
+        <div class="actions">
+            <a class="btn btn-primary btn-sm" href="{{ route('poem.add') }}">Add</a>
+        </div>
+    @endif
+@endsection
+
 @section('pagecontent')
     @unless(Auth::check())
         Please login to see a list of your poems.
     @else
-        <ul class="poems">
-            @foreach ( $poems as $poem )
-                <a href="{{ route('poem', ['id' => $poem->id]) }}">
-                    <li class="poem">
-                        <span class="poem__title">{{ $poem->title }}</span> -
-                        @if (! empty($poem->authors()->get()))
-                            @foreach ($poem->authors()->get() as $author)
-                                {{ $author->getPreferredName() }}@if (!$loop->last), @endif
-                            @endforeach
-                        @else
-                            Anon
-                        @endif
-                    </li>
-                </a>
-            @endforeach
-        </ul>
+        @empty($poems->count())
+            <div class="no-poems information">
+                <p>You have no poems added. Go for it!</p>
+            </div>
+        @else
+            <ul class="poems">
+                @foreach ( $poems as $poem )
+                    <a href="{{ route('poem', ['id' => $poem->id]) }}">
+                        <li class="poem">
+                            <span class="poem__title">{{ $poem->title }}</span> -
+                            @if (! empty($poem->authors()->get()))
+                                @foreach ($poem->authors()->get() as $author)
+                                    {{ $author->getPreferredName() }}@if (!$loop->last), @endif
+                                @endforeach
+                            @else
+                                Anon
+                            @endif
+                        </li>
+                    </a>
+                @endforeach
+            </ul>
+        @endempty
     @endunless
 @endsection
